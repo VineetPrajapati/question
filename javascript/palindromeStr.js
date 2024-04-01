b363ffa150cd437d6106514cd5daa4bc87652a2a
Vineet <infinite2202@gmail.com>
2024-04-01 19:39:32 +0530

    palindromeStr

--- javascript/palindromeStr.js
+++ javascript/palindromeStr.js
@@ -1,8 +1,10 @@
-const checkPalindrome = () => {
-    
+const checkPalindrome = (str) => {
+    word = str.toLowerCase().replace(/\W/g, "")
+    reverseStr = word.split("").reverse().join("")
+    return word === reverseStr ? true : false
 }
-
 
 console.log(checkPalindrome("ABCDCBA"))
 console.log(checkPalindrome("Hello"))
-console.log(checkPalindrome("Racer"))
+console.log(checkPalindrome("12321"))
+console.log(checkPalindrome("A man is running"))
