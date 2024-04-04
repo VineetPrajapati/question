4f5a0eb794d2aeebfc31a6f050dfcbd10bc28506
Vineet <infinite2202@gmail.com>
2024-04-04 10:26:13 +0530

    maxNumber

--- javascript/maxNumber.js
+++ javascript/maxNumber.js
@@ -1,9 +1,5 @@
-const findMax = (arr) =>{
-
+const findMax = (arr) => {
     return Math.max(...arr)
-    
 }
-
-
-console.log(findMax([4,8,24,5,23,8]));
-console.log(findMax([50,34,68,33,68,99]));
+console.log(findMax([4, 8, 24, 5, 23, 8]));
+console.log(findMax([50, 34, 68, 33, 68, 99]));
