0f0807bc5f416ea6ab1cf2d87143a079001035f0
Vineet <infinite2202@gmail.com>
2024-04-06 09:56:21 +0530

    averagaNumber

--- javascript/average.js
+++ javascript/average.js
@@ -1,7 +1,5 @@
 const findAvg = (arr) => {
     total = arr.reduce((accum, curNum) => accum + curNum, 0);
-    return total/arr.length;
+    return total / arr.length;
 }
-
-
-console.log(findAvg([4,6,10,4,2]));
+console.log(findAvg([4, 6, 10, 4, 2]));
